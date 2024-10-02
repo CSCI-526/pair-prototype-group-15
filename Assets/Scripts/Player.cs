f5e16bdb1e1b717a5701e3f2b06c5b2c0f6c07f1
ManasP160 <manasvir@usc.edu>
2024-10-02 11:45:23 -0700

    Changed PlayerScript

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,18 +10,23 @@ public class Player : MonoBehaviour
 
     public healthBar healthBar;  
     private bool isDead; 
+    private float healthDecreaseInterval = 5f; // used for decreasing health with respect to time
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth ; 
+        currentHealth = 50 ; 
         healthBar.SetMaxHealth(maxHealth);
+        healthBar.SetHealth(currentHealth);
+        StartCoroutine(DecreaseHealthOverTime());
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Implement Collison detection here and change the health point accordingly
         if(Input.GetKeyDown(KeyCode.Space)){
-            TakeDamage(10);
+
+            TakeDamage(10); // Increasing Health by 10 points when ever player is hit
         }
         if(currentHealth <= 0 && !isDead)
         {
@@ -29,11 +34,38 @@ public class Player : MonoBehaviour
             gameManager.gameOver();
             Debug.Log("Dead");
         }
+       
         
     }
 
     void TakeDamage(int damage){
-        currentHealth -= damage; 
+        currentHealth += damage;
+
+        if (currentHealth> maxHealth)
+        {
+            currentHealth = maxHealth;   // Visual changes can be added here for when current health exceeds maxhealth
+            isDead = true ; 
+            gameManager.gameOver();
+            Debug.Log("Dead due to excess engergy");
+        }
+        else 
+        {
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ensure health stays within bounds 
+        }
+
         healthBar.SetHealth(currentHealth);
     }
+
+    //Decreasing Health over Time
+    IEnumerator DecreaseHealthOverTime()
+    {
+        while(!isDead)
+        {
+            yield return new WaitForSeconds(healthDecreaseInterval);
+            currentHealth -= 10; // Changes for Damage with time can be done here
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ensure health stays within bounds
+            healthBar.SetHealth(currentHealth);
+        }
+    }
+
 }
